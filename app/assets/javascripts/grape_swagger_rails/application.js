43eaa6b3249cd483252e2771b3e23cf0ce862393
Leonard Teo <leonard@ballistiq.com>
2016-02-10 03:38:04 -0500

    Use precompiled version of application.js for faster asset compilation

--- app/assets/javascripts/grape_swagger_rails/application.js
+++ app/assets/javascripts/grape_swagger_rails/application.js
@@ -1,12 +1,14 @@
-//= require ./jquery-1.8.0.min
-//= require ./jquery.slideto.min
-//= require ./jquery.wiggle.min
-//= require ./jquery.ba-bbq.min
-//= require ./handlebars-2.0.0
-//= require ./marked
-//= require ./underscore-min
-//= require ./backbone-min
-//= require ./swagger-ui.min
-//= require ./highlight.7.3.pack
-//= require ./swagger-oauth
-//= require ./base64
+// Don't precompile any of these....
+// We skip the asset pipeline and save the precompiled version in the app public folder directly
+// //= require ./jquery-1.8.0.min
+// //= require ./jquery.slideto.min
+// //= require ./jquery.wiggle.min
+// //= require ./jquery.ba-bbq.min
+// //= require ./handlebars-2.0.0
+// //= require ./marked
+// //= require ./underscore-min
+// //= require ./backbone-min
+// //= require ./swagger-ui.min
+// //= require ./highlight.7.3.pack
+// //= require ./swagger-oauth
+// //= require ./base64
